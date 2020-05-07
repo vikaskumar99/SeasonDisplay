ae119df82c6f9b0aa47e3a49f11998e4346d0a00
Vikas Kumar L <vikki6842@gmail.com>
2020-05-07 21:42:53 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
+import ErrorMessage from './ErrorMessage';
+/*const App = () =>
+{
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return <div>HEllo!!!!!!!!!!!!</div>;
+};
+*/
+class App extends React.Component
+{
+    state = {lat: null, error:''};
+
+    componentDidMount()
+    {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat : position.coords.latitude})
+            },
+            (err) => {
+                this.setState({error: err.message});
+            }
+        );
+    }
+    render()
+    {
+        if(this.state.error && !this.state.lat)
+            return <ErrorMessage />
+        if(!this.state.error && this.state.lat)
+            return <SeasonDisplay lat={this.state.lat}/>
+        return <Loader />
+    }
+}
+
+
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
